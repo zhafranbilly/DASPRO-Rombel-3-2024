6ba57c0d78f8820076dfa7179e2925fdd53c7117
AhsanRony-ron <ahsanronykusuma@gmail.com>
2024-10-08 09:54:41 +0700

    Update 3-I.cpp

---
+++ Tugas TLX/2405100088_AHSAN RONY KUSUMA/3-I.cpp
@@ -0,0 +1,13 @@
+/* Soal
+Berapa kalikah tanda bintang tercetak pada potongan program berikut?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total = 0; 
+        while (total < 2) {
+        cout << "*" << endl;
+    }
+}
