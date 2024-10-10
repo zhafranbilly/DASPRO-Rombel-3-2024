51b1a735c46f476ab7e9034c6409230ed9331892
AQUADITZ <143810972+AQUADITZ@users.noreply.github.com>
2024-10-10 19:52:02 +0700

    Update 1-C.cpp

---
+++ Tugas TLX/2405100078_ADITYA RIZKY AL'KHOLIQ/1-C.cpp
@@ -0,0 +1,16 @@
+// Sekarang, Pak Dengklek ingin sebuah program yang keluarannya adalah:
+
+// Pak Dengklek memiliki 75 ekor bebek.
+// Terdapat tanda titik-titik (...) pada program di sebelah kanan. Isilah dengan sebuah bilangan agar keluaran di atas tercetak.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Pak Dengklek memiliki " << 75 << " ekor bebek." << std::endl;
+}
+
+// Pada C++, nilai "75" adalah string, sedangkan nilai 75 adalah bilangan (bulat).
+
+// Jika dicetak, keduanya menghasilkan keluaran yang sama, yakni teks 75.
+
+// Akan tetapi, nilai berupa bilangan memungkinkan kita untuk melakukan perhitungan ekspresi aritmetika, seperti yang akan dibahas pada soal berikutnya.
