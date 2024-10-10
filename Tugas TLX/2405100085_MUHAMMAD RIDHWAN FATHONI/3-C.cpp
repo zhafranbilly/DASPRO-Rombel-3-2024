37304ce87eb3c0a23a4e47d30d5f95f847561feb
Rx2wn <muhammadridhwanfathoni@gmail.com>
2024-10-10 21:39:59 +0700

    Update 3-C.cpp

---
+++ Tugas TLX/2405100085_MUHAMMAD RIDHWAN FATHONI/3-C.cpp
@@ -0,0 +1,33 @@
+//Soal
+//Lengkapilah program di sebelah kanan, agar potongan kode di dalam while dijalankan tiga kali, sesuai dengan materi yang telah kita pelajari.
+
+
+//kode
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+    int total_baris = 0;
+    
+    while (total_baris < 3) {
+        luas_kandang += 7;
+        cout << luas_kandang << endl;
+        total_baris += 1;
+
+    }
+}
+*/
+
+//penjelasan
+/*
+sama seperti soal sebelumnya atau 3-B program ini bertujuan untuk menghitung total luas kandang pak Dengklek pada 3 bulan ke depan
+hanya saja program ini lebih simple karna kita tidak pelu menulis tiga kali untuk mendapat keluaran kode nya
+pada program ini kita menggunakan "while' untuk mempermudah kita
+Perulangan while akan terus berjalan selama nilai total_baris kurang dari 3. Jadi, perulangan ini akan dijalankan sebanyak 3 kali (ketika total_baris bernilai 0, 1, dan 2).
+untuk out put nya sendiri sama dengan soal 3-B yaitu:
+19
+26
+33
+*/
