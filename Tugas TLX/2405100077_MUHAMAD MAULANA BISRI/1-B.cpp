814fd3dd9cf997d47497b8721d36f2c4ebb32ea7
bisridiskoin <bisrimaulana1@students.unnes.ac.id>
2024-10-10 22:43:56 +0700

    Update 1-B.cpp

---
+++ Tugas TLX/2405100077_MUHAMAD MAULANA BISRI/1-B.cpp
@@ -0,0 +1,21 @@
+//Soal
+//Kali ini, Pak Dengklek meminta Anda untuk mencetak keluaran berikut:
+
+//Halo, dunia!
+//Aku semangat belajar C++!
+//Lengkapilah program di sebelah kanan agar menghasilkan keluaran tersebut!
+
+#include <iostream>
+
+int main() {
+    std::cout << "Halo, dunia!" << std::endl;
+    std::cout << "Aku semangat belajar C++!" << std::endl;
+
+    return 0;
+}
+
+//Program ini mencetak dua pesan ke konsol: "Halo, dunia!" diikuti oleh "Aku semangat belajar C++!". Ini adalah contoh dasar penggunaan output di C++. Jika kamu menjalankannya, kamu akan melihat kedua pesan tersebut muncul di layar.
+
+
+
+ 
