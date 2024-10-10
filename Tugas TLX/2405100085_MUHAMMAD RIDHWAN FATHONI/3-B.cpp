379ea81ab86f8833179547ac7c1cd35022bd98d2
Rx2wn <muhammadridhwanfathoni@gmail.com>
2024-10-10 21:24:57 +0700

    Update 3-B.cpp

---
+++ Tugas TLX/2405100085_MUHAMMAD RIDHWAN FATHONI/3-B.cpp
@@ -0,0 +1,51 @@
+/* 
+Soal
+Pak Dengklek memiliki sebuah kandang bebek seluas 
+12
+12 meter persegi.
+
+Setiap bulannya, Pak Dengklek ingin membeli sebuah kandang baru. Karena bebek-bebeknya terus berkembang biak, Pak Dengklek ingin agar setiap kandang baru yang dibeli berukuran 
+7
+7 meter persegi lebih luas daripada kandang sebelumnya.
+
+Lengkapilah program di sebelah kanan agar mengeluarkan ukuran luas kandang-kandang (dalam meter persegi) sesuai urutan pembelian yang harus dilakukan, satu baris untuk setiap kandang, untuk tiga bulan ke depan.
+*/
+
+//kode
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+
+    // bulan pertama
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    // bulan kedua
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+
+    // bulan ketiga
+    luas_kandang += 7;
+    cout << luas_kandang << endl;
+}
+*/
+
+//penjelasan
+/*
+Program ini bertujuan untuk menghitung luas kandang pak Dengklek 3 bulan ke depan
+
+dengan contoh, pada bulan pertama luas kandang pak Dengklek bertambah 7 meter,dan hasil akhirnya adalah 19.
+
+pada bulan kedua, kandang pak Dengklek akan bertambah 7 meter lagi dan menjadi 26.
+
+dan pada bulan ketiga, luas kandang pak Dengklek adalah 33.
+
+jadi hasil out put pada kode di atas adalah:
+19
+26
+33
+
+*/
